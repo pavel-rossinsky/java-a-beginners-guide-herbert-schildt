68d875e25524e943c214c046a961056e44c8eedc
prossinsky <pavel.rossinsky@planet-sports.com>
2020-04-16 18:22:47 +0200

    #6 Fix the definition, it's not C++

--- src/main/java/chapter1/Example2.java
+++ src/main/java/chapter1/Example2.java
@@ -1,5 +1,5 @@
 class Example2 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int var1;
         int var2;
 
---
+++ src/main/java/chapter1/Example3.java
@@ -0,0 +1,5 @@
+class Example3 {
+    public static void main(String[] args) {
+        System.out.print("hello java");
+    }
+}
