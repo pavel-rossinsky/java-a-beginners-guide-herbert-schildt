991dd9fe4f6adbfe802df9b34d59867a768ef889
prossinsky <pavel.rossinsky@planet-sports.com>
2020-04-16 18:30:18 +0200

    #6 Accomplished example 3

--- src/main/java/chapter1/Example3.java
+++ src/main/java/chapter1/Example3.java
@@ -1,5 +1,19 @@
 class Example3 {
     public static void main(String[] args) {
-        System.out.print("hello java");
+        int var;
+        double x;
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("Original value of var: " + var);
+        System.out.println("Original value of x: " + x);
+        System.out.println();
+
+        var = var / 4;
+        x = x / 4;
+
+        System.out.println("var after division: " + var);
+        System.out.println("x after division: " + x);
     }
 }
