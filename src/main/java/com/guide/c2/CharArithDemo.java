2481ae252b77529c23bf6e2195e45e0d54b5abbb
prossinsky <pavel.rossinsky@planet-sports.com>
2020-04-21 19:47:18 +0200

    Add the CharArithDemo class

---
+++ src/main/java/com/guide/c2/CharArithDemo.java
@@ -0,0 +1,21 @@
+package com.guide.c2;
+
+/**
+ * Character variables can be handled like integers
+ */
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++;
+
+        System.out.println("ch is now " + ch);
+
+        ch = 90;
+
+        System.out.println("ch is now " + ch);
+    }
+}
