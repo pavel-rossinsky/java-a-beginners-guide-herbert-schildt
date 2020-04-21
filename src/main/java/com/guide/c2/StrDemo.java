a5803b12452ea9ff2d9acd33668f2eb55636dd59
prossinsky <pavel.rossinsky@planet-sports.com>
2020-04-21 21:00:18 +0200

    Add the StrDemo class

---
+++ src/main/java/com/guide/c2/StrDemo.java
@@ -0,0 +1,10 @@
+package com.guide.c2;
+
+// Demonstrate escape sequences in strings
+public class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("First line\nSecond line");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
