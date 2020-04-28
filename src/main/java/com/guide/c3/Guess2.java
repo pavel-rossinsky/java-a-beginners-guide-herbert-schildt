9553cfe3db05ecd7e0a6bfdbb8a73c4a03433cdf
prossinsky <po.rossinsky@gmail.com>
2020-04-28 09:18:48 +0200

    Add the Guess2 class

---
+++ src/main/java/com/guide/c3/Guess2.java
@@ -0,0 +1,18 @@
+package com.guide.c3;
+
+// Guess the letter game.
+public class Guess2 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+
+        char ch, answer = 'K';
+
+        System.out.println("I'm thinking of a letter between A and Z.");
+        System.out.print("Can you guess it: ");
+
+        ch = (char) System.in.read(); // read a char from tge keyboard
+
+        if (ch == answer) System.out.println("*** Right ***");
+        else System.out.println("...Sorry, you're wrong.");
+    }
+}
