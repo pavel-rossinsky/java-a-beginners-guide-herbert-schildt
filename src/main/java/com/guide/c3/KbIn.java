b43ca03de893abc4977ad6cbb1b17e215a17d9d8
prossinsky <po.rossinsky@gmail.com>
2020-04-28 09:10:03 +0200

    Correct the comment

--- src/main/java/com/guide/c3/KbIn.java
+++ src/main/java/com/guide/c3/KbIn.java
@@ -11,7 +11,7 @@ public class KbIn {
 
         System.out.print("Press a key followed by ENTER: ");
 
-        ch = (char) System.in.read(); // Read a character from keyboard
+        ch = (char) System.in.read(); // Read a character from the keyboard
 
         System.out.println("Your key is: " + ch);
     }
