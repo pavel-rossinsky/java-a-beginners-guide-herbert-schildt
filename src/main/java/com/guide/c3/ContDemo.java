6cfe520d19e6d2f38119db3a09b5c6dc76d581ee
prossinsky <po.rossinsky@gmail.com>
2020-04-29 09:37:03 +0200

    Update the ContDemo class

--- src/main/java/com/guide/c3/ContDemo.java
+++ src/main/java/com/guide/c3/ContDemo.java
@@ -1,16 +1,14 @@
 package com.guide.c3;
 
-// Use continue with a label.
+// Use continue.
 public class ContDemo {
     public static void main(String[] args) {
-outerloop:
-        for (int i = 1; i < 10; i++)
-        {
-            System.out.println("\nOuter loop pass " + i + ", Inner loop: ");
-            for (int j = 1; j < 10; j++) {
-                if (j == 5) continue outerloop;
-                System.out.print(j);
-            }
+        int i;
+
+        // print even numbers between 0 and 100
+        for (i = 1; i <= 100; i++) {
+            if ((i % 2) != 0) continue;
+            System.out.println(i);
         }
     }
 }
