019cb79b07c149832ed4c9f82b497be601d43e76
prossinsky <po.rossinsky@gmail.com>
2020-04-30 21:09:17 +0200

    Add the AddMeth class

--- src/main/java/com/guide/c4/Vehicle.java
+++ src/main/java/com/guide/c4/Vehicle.java
@@ -4,4 +4,8 @@ public class Vehicle {
     int passengers; // number of passengers
     int fuelcap; // fuel capacity in gallons
     int mpg; // fuel consumption in miles per gallon
+
+    void range() {
+        System.out.println("Range is " + fuelcap * mpg);
+    }
 }
