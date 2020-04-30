8155e08a0251d68103d8e226b6a80b7dbe41fc0b
prossinsky <po.rossinsky@gmail.com>
2020-04-30 20:52:33 +0200

    Add the Vehicle class

---
+++ src/main/java/com/guide/c4/Vehicle.java
@@ -0,0 +1,7 @@
+package com.guide.c4;
+
+public class Vehicle {
+    int passengers; // number of passengers
+    int fuelcap; // fuel capacity in gallons
+    int mpg; // fuel consumption in miles per gallon
+}
