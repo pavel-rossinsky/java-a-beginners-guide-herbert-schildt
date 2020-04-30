8cedca8ed20ca5866670ff479824a2d3dc80fd24
prossinsky <po.rossinsky@gmail.com>
2020-04-30 09:05:00 +0200

    Add the SelfTest4 class

---
+++ src/main/java/com/guide/c3/SelfTest4.java
@@ -0,0 +1,12 @@
+package com.guide.c3;
+
+/**
+ * Show the for statement for a loop that count from 1000 to 0 by -2.
+ */
+public class SelfTest4 {
+    public static void main(String[] args) {
+        for (int j = 1000; j >= 0; j -= 2) {
+            System.out.println(j);
+        }
+    }
+}
