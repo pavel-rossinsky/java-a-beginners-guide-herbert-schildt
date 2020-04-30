1bd2ce1a4ddaecee2e24ba8c9543643e2ac244d5
prossinsky <po.rossinsky@gmail.com>
2020-04-30 21:00:01 +0200

    Add the TwoVehicles class

---
+++ src/main/java/com/guide/c4/TwoVehicles.java
@@ -0,0 +1,29 @@
+package com.guide.c4;
+
+// This program creates two vehicle objects.
+public class TwoVehicles {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+
+        int range1, range2;
+
+        // assign values to fields in minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        // assign values to fields in sportscar
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+
+        // compute the ranges assuming a full tank of gas
+        range1 = minivan.fuelcap * minivan.mpg;
+        range2 = sportscar.fuelcap * sportscar.mpg;
+
+        System.out.println("Minivan can carry " + minivan.passengers + " with a range of " + range1);
+
+        System.out.println("Sportscar can carry " + sportscar.passengers + " with a range of " + range2);
+    }
+}
