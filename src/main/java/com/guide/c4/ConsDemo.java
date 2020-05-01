b119be9e1145fb7a2a0a47496ddd7446d6973a08
prossinsky <po.rossinsky@gmail.com>
2020-05-01 10:55:55 +0200

    Add the ConsDemo class

---
+++ src/main/java/com/guide/c4/ConsDemo.java
@@ -0,0 +1,10 @@
+package com.guide.c4;
+
+public class ConsDemo {
+    public static void main(String[] args) {
+        MyClass t1 = new MyClass();
+        MyClass t2 = new MyClass();
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
