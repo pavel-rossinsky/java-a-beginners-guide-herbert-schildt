7e23f315c8f4f3f5e9aba2e1b8558e6cd3f534aa
prossinsky <po.rossinsky@gmail.com>
2020-05-03 12:55:17 +0200

    Complete the ErrMsg class

---
+++ src/main/java/com/guide/c6/ErrMsg.java
@@ -0,0 +1,10 @@
+package com.guide.c6;
+
+public class ErrMsg {
+    public static void main(String[] args) {
+        ErrorMsg err = new ErrorMsg();
+
+        System.out.println(err.getErrorMsg(2));
+        System.out.println(err.getErrorMsg(19));
+    }
+}
