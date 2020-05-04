1dabf083a2cb47d46952ff92f523408dd48c8d5a
prossinsky <po.rossinsky@gmail.com>
2020-05-04 22:00:27 +0200

    Complete the TypeConv class

--- src/main/java/com/guide/c6/Overload2.java
+++ src/main/java/com/guide/c6/Overload2.java
@@ -5,6 +5,10 @@ package com.guide.c6;
  * overloaded method resolution.
  */
 public class Overload2 {
+    void f(byte x) {
+        System.out.println("Inside f(byte): " + x);
+    }
+
     void f(int x) {
         System.out.println("Inside f(int): " + x);
     }
