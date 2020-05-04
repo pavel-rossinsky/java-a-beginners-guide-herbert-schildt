0619e0df098d4efb059da09acd562e4d2e754b91
prossinsky <po.rossinsky@gmail.com>
2020-05-04 22:53:13 +0200

    Complete the Recursion class

---
+++ src/main/java/com/guide/c6/Recursion.java
@@ -0,0 +1,19 @@
+package com.guide.c6;
+
+public class Recursion {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+
+        System.out.println("Factorials using recursive method.");
+        System.out.println("Factorial of 3 is " + f.factR(3));
+        System.out.println("Factorial of 4 is " + f.factR(4));
+        System.out.println("Factorial of 5 is " + f.factR(5));
+        System.out.println();
+
+        System.out.println("Factorial using iterative method.");
+        System.out.println("Factorial of 3 is " + f.factI(3));
+        System.out.println("Factorial of 4 is " + f.factI(4));
+        System.out.println("Factorial of 5 is " + f.factI(5));
+        System.out.println();
+    }
+}
