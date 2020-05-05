5de218387653d016ddfc8613f0e47361b68b6cb9
prossinsky <po.rossinsky@gmail.com>
2020-05-05 18:28:22 +0200

    Complete the SelfTest3 class

--- src/main/java/com/guide/c6/SelfTest3.java
+++ src/main/java/com/guide/c6/SelfTest3.java
@@ -14,5 +14,16 @@ public class SelfTest3 {
         do {
             c = myStack.pop();
         } while (c != 0);
+
+        Stack myStack2 = new Stack(4);
+        myStack2.put('H');
+        myStack2.put('i');
+        myStack2.put('!');
+        myStack2.pop();
+        myStack2.put('?');
+
+        do {
+            c = myStack2.pop();
+        } while (c != 0);
     }
 }
--- src/main/java/com/guide/c6/Stack.java
+++ src/main/java/com/guide/c6/Stack.java
@@ -3,34 +3,33 @@ package com.guide.c6;
 // Create a Stack
 public class Stack {
     private char[] q; // this array holds the stack
-    private int putloc, getloc; // the put and get indices
+    private int loc; // the put and pop index
 
     // Construct a Stack
     Stack(int size) {
         q = new char[size]; // allocate memory for stack
-        getloc = putloc = size;
+        loc = size;
 
     }
 
     void put(char ch) {
-        if (putloc == 0) {
+        if (loc == 0) {
             System.out.println(" - Stack is full.");
             return;
         }
 
         System.out.println(" - put " + ch);
 
-        q[--putloc] = ch;
-        getloc = putloc;
+        q[--loc] = ch;
     }
 
     char pop() {
-        if (getloc == q.length) {
+        if (loc == q.length) {
             System.out.println(" - Stack is empty.");
             return (char) 0;
         }
 
-        char ch = q[getloc++];
+        char ch = q[loc++];
 
         System.out.println(" - pop " + ch);
 
