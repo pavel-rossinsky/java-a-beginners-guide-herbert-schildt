66179606a4ab85008ecd4581bd844176b0186e12
prossinsky <po.rossinsky@gmail.com>
2020-05-05 13:11:08 +0200

    Complete the VarArgs2 class

---
+++ src/main/java/com/guide/c6/VarArgs2.java
@@ -0,0 +1,20 @@
+package com.guide.c6;
+
+// Use varargs with standard arguments.
+public class VarArgs2 {
+    static void vaTest(String msg, int ... v) {
+        System.out.println(msg + v.length);
+        System.out.println("Contents: ");
+
+        for (int i = 0; i < v.length; i++)
+            System.out.println(" arg " + i + ": " + v[i]);
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest("One vararg: ", 10);
+        vaTest("Three varargs: ", 1, 2, 3);
+        vaTest("No varargs: ");
+    }
+}
