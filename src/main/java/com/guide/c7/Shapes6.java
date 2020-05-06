147969b68ed0fa7c1346d08214950fd48ffc24a4
prossinsky <po.rossinsky@gmail.com>
2020-05-06 21:57:18 +0200

    Complete the Shapes6 class

---
+++ src/main/java/com/guide/c7/Shapes6.java
@@ -0,0 +1,22 @@
+package com.guide.c7;
+
+public class Shapes6 {
+    public static void main(String[] args) {
+        ColorTriangle t1 = new ColorTriangle("Blue", "outlined", 8.0, 12.0);
+        ColorTriangle t2 = new ColorTriangle("Red", "filled", 2.0, 2.0);
+
+        System.out.println("Info for t1: ");
+        t1.showStyle();
+        t1.showDim();
+        t1.showColor();
+        System.out.println("Area is " + t1.area());
+
+        System.out.println();
+
+        System.out.println("Info for t2: ");
+        t2.showStyle();
+        t2.showDim();
+        t2.showColor();
+        System.out.println("Area is " + t2.area());
+    }
+}
