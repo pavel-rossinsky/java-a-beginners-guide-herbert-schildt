2522c8d328eebf36ddd544f171b0d8c06fa66a9b
prossinsky <po.rossinsky@gmail.com>
2020-05-06 08:56:33 +0200

    Complete the TwoDShape class

---
+++ src/main/java/com/guide/c7/TwoDShape.java
@@ -0,0 +1,11 @@
+package com.guide.c7;
+
+// A class for two-dimensional hierarchy.
+public class TwoDShape {
+    double width;
+    double height;
+
+    void showDim() {
+        System.out.println("Width and height are " + width + " and " + height);
+    }
+}
