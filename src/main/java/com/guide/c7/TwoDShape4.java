82ad9e251c145e6f97124d59b68651ad2a5849ed
prossinsky <po.rossinsky@gmail.com>
2020-05-06 20:29:54 +0200

    Complete the TwoDShape4 class

--- src/main/java/com/guide/c7/TwoDShape4.java
+++ src/main/java/com/guide/c7/TwoDShape4.java
@@ -1,8 +1,14 @@
 package com.guide.c7;
 
+// Add constructors to the class.
 public class TwoDShape4 {
     private double width;
     private double height;
+
+    TwoDShape4(double w, double h) {
+        width = w;
+        height = h;
+    }
 
     // Accessor methods for with and height.
     double getWidth() {
