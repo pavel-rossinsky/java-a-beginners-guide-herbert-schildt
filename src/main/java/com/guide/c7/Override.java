668d148a2b23240f03542d5131d12e4cdb284bc3
prossinsky <po.rossinsky@gmail.com>
2020-05-07 22:58:33 +0200

    Complete the Override class

---
+++ src/main/java/com/guide/c7/Override.java
@@ -0,0 +1,9 @@
+package com.guide.c7;
+
+public class Override {
+    public static void main(String[] args) {
+        B3 subOb = new B3(1, 2, 3);
+
+        subOb.show(); // this calls show in B
+    }
+}
