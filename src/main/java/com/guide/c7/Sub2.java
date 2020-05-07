d7be0e0d05742c8e9af62c9afc3ff4c0298e1c06
prossinsky <po.rossinsky@gmail.com>
2020-05-07 23:23:16 +0200

    Complete the Sup, Sub1, Sub2 classes

---
+++ src/main/java/com/guide/c7/Sub2.java
@@ -0,0 +1,7 @@
+package com.guide.c7;
+
+public class Sub2 extends Sup {
+    void who() {
+        System.out.println("who() is Sub2");
+    }
+}
---
+++ src/main/java/com/guide/c7/Sup.java
@@ -0,0 +1,7 @@
+package com.guide.c7;
+
+public class Sup {
+    void who() {
+        System.out.println("who() in Sup");
+    }
+}
