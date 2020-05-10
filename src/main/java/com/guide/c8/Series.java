51705275a44406a00bd4998e68caa249ac9ea070
prossinsky <po.rossinsky@gmail.com>
2020-05-10 22:39:12 +0200

    Complete the UseBook2 interface

---
+++ src/main/java/com/guide/c8/Series.java
@@ -0,0 +1,7 @@
+package com.guide.c8;
+
+public interface Series {
+    int getNext(); // return next number in series
+    void reset(); // restart
+    void setStart(int x); // set starting value
+}
