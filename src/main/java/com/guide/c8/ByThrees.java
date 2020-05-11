5fa571a9a6e46b330687b9beee016458e2ea6fc4
prossinsky <po.rossinsky@gmail.com>
2020-05-11 22:55:56 +0200

    Complete the ByThrees class

---
+++ src/main/java/com/guide/c8/ByThrees.java
@@ -0,0 +1,25 @@
+package com.guide.c8;
+
+public class ByThrees implements Series {
+    int start;
+    int val;
+
+    ByThrees() {
+        start = 0;
+        val = 0;
+    }
+
+    public int getNext() {
+        val += 3;
+        return val;
+    }
+
+    public void reset() {
+        val = start;
+    }
+
+    public void setStart(int x) {
+        start = x;
+        val = x;
+    }
+}
