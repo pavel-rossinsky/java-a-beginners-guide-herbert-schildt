c35e4ccaaaee368fb17ec653ce83875f0ce53fe6
prossinsky <po.rossinsky@gmail.com>
2020-05-11 23:14:59 +0200

    Complete the ICharQ interface

---
+++ src/main/java/com/guide/c8/ICharQ.java
@@ -0,0 +1,10 @@
+package com.guide.c8;
+
+// A character queue interface.
+public interface ICharQ {
+    // Put a character into the queue.
+    void put(char ch);
+
+    // Get a character from the queue.
+    char get();
+}
