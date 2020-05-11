e05876d747aa57ec76a5a824a5a57a507bf260a2
prossinsky <po.rossinsky@gmail.com>
2020-05-11 22:39:46 +0200

    Complete the ByTwos class

---
+++ src/main/java/com/guide/c8/ByTwos.java
@@ -0,0 +1,25 @@
+package com.guide.c8;
+
+public class ByTwos implements Series {
+    int start;
+    int val;
+
+    ByTwos() {
+        start = 0;
+        val = 0;
+    }
+
+    public int getNext() {
+        val += 2;
+        return val;
+    }
+
+    public void reset() {
+        val = start;
+    }
+
+    public void setStart(int x) {
+        start = x;
+        val = x;
+    }
+}
