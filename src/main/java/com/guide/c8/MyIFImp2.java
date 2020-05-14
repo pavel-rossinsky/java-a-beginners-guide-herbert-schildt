1c3abe57ff95096375d9c3013e3d9033a8181b04
prossinsky <po.rossinsky@gmail.com>
2020-05-14 18:40:03 +0200

    Complete the MyIFImp2 class

---
+++ src/main/java/com/guide/c8/MyIFImp2.java
@@ -0,0 +1,12 @@
+package com.guide.c8;
+
+// Here, implementations for both getUserID() and getAdminID() are provided.
+public class MyIFImp2 implements MyIF {
+    public int getUserID() {
+        return 100;
+    }
+
+    public int getAdminID() {
+        return 42;
+    }
+}
