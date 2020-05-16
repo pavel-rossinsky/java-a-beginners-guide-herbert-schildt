03e205fe8fd245e2ca0235516b76b6d0049bc6de
prossinsky <po.rossinsky@gmail.com>
2020-05-16 12:01:33 +0200

    Complete the Rethrow class

---
+++ src/main/java/com/guide/c9/Rethrow.java
@@ -0,0 +1,23 @@
+package com.guide.c9;
+
+// Rethrow an exception.
+public class Rethrow {
+    public static void getException() {
+        // Here, numer is longer that denom.
+        int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
+        int[] denom = { 2, 0, 4, 4, 0, 8 };
+
+        for (int i = 0; i < numer.length; i++) {
+            try {
+                System.out.println(numer[i] + " / " + denom[i] + " is " + numer[i]/denom[i]);
+            } catch (ArithmeticException exc) {
+                // catch the exception
+                System.out.println("Can't divide by Zero!");
+            } catch (ArrayIndexOutOfBoundsException exc) {
+                // catch the exception
+                System.out.println("No matching element found.");
+                throw exc; // rethrow the exception
+            }
+        }
+    }
+}
