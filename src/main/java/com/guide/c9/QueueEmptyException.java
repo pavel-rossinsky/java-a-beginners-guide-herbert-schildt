c118e245ef109e4aa2d8b1a9d45ac7aae6884a67
prossinsky <po.rossinsky@gmail.com>
2020-05-16 13:25:34 +0200

    Complete the QueueEmptyException class

--- src/main/java/com/guide/c9/QueueEmptyException.java
+++ src/main/java/com/guide/c9/QueueEmptyException.java
@@ -1,7 +1,7 @@
 package com.guide.c9;
 
 // An exception for queue-empty errors.
-public class QueueEmptyException {
+public class QueueEmptyException extends Exception {
     public String toString() {
         return "\nQueue is empty.";
     }
