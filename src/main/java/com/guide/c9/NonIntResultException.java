baed27fa732a0413c64d54b822e75e22c4bb287b
prossinsky <po.rossinsky@gmail.com>
2020-05-16 13:07:19 +0200

    Complete the NonIntResultException class

---
+++ src/main/java/com/guide/c9/NonIntResultException.java
@@ -0,0 +1,16 @@
+package com.guide.c9;
+
+// Use a custom exception.
+public class NonIntResultException extends Exception {
+    int n;
+    int d;
+
+    NonIntResultException(int i, int j) {
+        n = i;
+        d = j;
+    }
+
+    public String toString() {
+        return "Result of " + n + " / " + d + " in non-integer.";
+    }
+}
